2f0cc29358269e7194c6513f12774332ee896c27
Claudia Zeledon <cjzeledon@gmail.com>
2017-08-08 16:35:01 -0400

    Commit

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 let body = document.querySelector("body");
+let recipe_section = document.querySelector("section");
 
 function JSonMyWay(dropthatAPI){
   return dropthatAPI.json();
@@ -9,24 +10,28 @@ function recipes(givetoAPI){
   console.log(givetoAPI.results[0].href);
   console.log(givetoAPI.results[0].thumbnail);
   console.log(givetoAPI.results[0].title);
-  let section = `
-  <div class="form_searchbox">
-    <form>
-    <input type="search" results="5" placeholder="Search Recipes">
-    <input type="submit">
-    </form>
-  </div>
-`;
-  // <div class="search_results">
-  //   for (let i=0; i > givetoAPI.results.length; i++){
-  //     <div class="search_giverecipes">
-  //     <a href="${givetoAPI.results[i].href}" target="_blank"><img src="${givetoAPI.results[i].thumbnail}" alt="${givetoAPI.results[i].title}"></a>
-  //     <p>${givetoAPI.results[i].title}</p>
-  //     </div>
-  //   }
-  // </div>
-  // `;
-  body.innerHTML = section;
+
+let something = '';
+
+  for (let i=0; i < givetoAPI.results.length; i++){
+    let section = `
+    <div class="search_results">
+        <div class="search_giverecipes">
+        <a href="${givetoAPI.results[i].href}" target="_blank"><img src="${givetoAPI.results[i].thumbnail}"></a>
+        <p>${givetoAPI.results[i].title}</p>
+        </div>
+    </div>
+    `;
+      if (givetoAPI.results[i].thumbnail === " "){
+        section = `
+        <img src="http://via.placeholder.com/140x100">
+        `;
+        something;
+      }
+something += section;
+  }
+
+recipe_section.innerHTML = something;
 }
 
 function ouch(err) {
