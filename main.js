f1e9fb8f9322d0c16b7a02045bdd05ba85f31bc3
Claudia Zeledon <cjzeledon@gmail.com>
2017-08-08 15:34:56 -0400

    Trying to work the for loop.

--- main.js
+++ main.js
@@ -1,22 +1,35 @@
-let section = document.querySelector("section");
+let body = document.querySelector("body");
 
 function JSonMyWay(dropthatAPI){
-  let results = dropthatAPI.json();
-  return results;
+  return dropthatAPI.json();
 }
 
-function recipes(dropthatAPI){
-  console.log(dropthatAPI);
-
-  let wrapper = `
-  <h1>Can you See Me?</h1>
-
-  `;
-  section.innerHTML = wrapper;
+function recipes(givetoAPI){
+  console.log(givetoAPI);
+  console.log(givetoAPI.results[0].href);
+  console.log(givetoAPI.results[0].thumbnail);
+  console.log(givetoAPI.results[0].title);
+  let section = `
+  <div class="form_searchbox">
+    <form>
+    <input type="search" results="5" placeholder="Search Recipes">
+    <input type="submit">
+    </form>
+  </div>
+`;
+  // <div class="search_results">
+  //   for (let i=0; i > givetoAPI.results.length; i++){
+  //     <div class="search_giverecipes">
+  //     <a href="${givetoAPI.results[i].href}" target="_blank"><img src="${givetoAPI.results[i].thumbnail}" alt="${givetoAPI.results[i].title}"></a>
+  //     <p>${givetoAPI.results[i].title}</p>
+  //     </div>
+  //   }
+  // </div>
+  // `;
+  body.innerHTML = section;
 }
 
 function ouch(err) {
-  console.log("Ouch! I'm not working right!");
   console.log(err);
 }
 
