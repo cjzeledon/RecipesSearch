0bf58b990562d73343933a738478dff9e19e94dd
Claudia Zeledon <cjzeledon@gmail.com>
2017-08-09 09:04:43 -0400

    Missing image placeholder now working. Adding an onclick eventlistener to search box--not yet working.

--- main.js
+++ main.js
@@ -1,5 +1,12 @@
 let body = document.querySelector("body");
 let recipe_section = document.querySelector("section");
+// create eventlistener for submit button
+// function that runs on event happening takes the text for the input box
+// puts that text into the url for the fetch
+
+search_load(find){
+  
+}
 
 function JSonMyWay(dropthatAPI){
   return dropthatAPI.json();
@@ -11,34 +18,53 @@ function recipes(givetoAPI){
   console.log(givetoAPI.results[0].thumbnail);
   console.log(givetoAPI.results[0].title);
 
-let something = '';
+  let something = '';
 
   for (let i=0; i < givetoAPI.results.length; i++){
-    let section = `
-    <div class="search_results">
-        <div class="search_giverecipes">
-        <a href="${givetoAPI.results[i].href}" target="_blank"><img src="${givetoAPI.results[i].thumbnail}"></a>
-        <p>${givetoAPI.results[i].title}</p>
-        </div>
-    </div>
-    `;
-      if (givetoAPI.results[i].thumbnail === " "){
-        section = `
-        <img src="http://via.placeholder.com/140x100">
-        `;
-        something;
-      }
-something += section;
-  }
 
-recipe_section.innerHTML = something;
+    let backupImage = `<img src="http://via.placeholder.com/140x100"></a>`;
+    if (givetoAPI.results[i].thumbnail === ''){
+      let missingPhoto = `
+          <div class="search_giverecipes">
+          <figure>
+          <a href="${givetoAPI.results[i].href}" target="_blank">
+          ${backupImage}
+          <figcaption>${givetoAPI.results[i].title}</figcaption>
+          <figure>
+          </div>
+          `;
+
+
+      something += missingPhoto;
+    } else {
+    let section = `
+        <div class="search_giverecipes">
+        <figure>
+        <a href="${givetoAPI.results[i].href}" target="_blank"><img src="${givetoAPI.results[i].thumbnail}"></a>
+        <figcaption>${givetoAPI.results[i].title}</figcaption>
+        <figure>
+        </div>
+        `;
+
+        // for (z = 0; z < givetoAPI.results.length; z++){
+          // let backupImage = `<img src="http://via.placeholder.com/140x100">`;
+          // if (givetoAPI.results[z].thumbnail === null){
+          //   givetoAPI.results[z].thumbnail = backupImage;
+          // }
+          // section += backupImage;
+        // }
+    something += section;
+  }
+  }
+    recipe_section.innerHTML = something;
 }
 
 function ouch(err) {
+  console.log("Ouch! I'm not working right!");
   console.log(err);
 }
 
-fetch ("http://recipepuppyproxy.herokuapp.com/api/?q=omelet")
+fetch ("http://recipepuppyproxy.herokuapp.com/api/?q=")
   .then(JSonMyWay)
   .then(recipes)
   .catch(ouch);
